2dc091989abd260a17aac94bd68c5ba07f1c0388
Kauê Kluska <kauekluska@gmail.com>
2026-04-28 18:50:13 -0300

    criação e integração do frontend, alteração na autenticação jwt para desenvolvimento

---
+++ frontend/src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import "./header.css"
+function Header() {
+  return (
+    <header>
+      <h1>Acapra</h1>
+      <nav>
+        <a href="/">Início</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

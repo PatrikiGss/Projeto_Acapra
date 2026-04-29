c3b7f9c2b74f9d41ed479fe70d6e4eb7545b650c
PatrikiGss <patriki7771@gmail.com>
2026-04-28 22:29:51 -0300

    estrututa basica do app gerenciamento

--- Acapra/gerenciamento/urls.py
+++ Acapra/gerenciamento/urls.py
@@ -1,6 +1,34 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import PerfilAdministrativoViewSet
 
-app_name='gerenciamento'
 
-urlpatterns = [
-]
+"""
+Arquivo responsável pelas rotas da app gerenciamento.
+
+====================================================
+Rotas geradas 
+====================================================
+
+/gerenciamento/perfil-administrativo/
+/gerenciamento/perfil-administrativo/{id}/
+
+com suporte a:
+
+GET
+POST
+PUT
+PATCH
+DELETE
+"""
+
+app_name = "gerenciamento"
+
+router = DefaultRouter()
+
+router.register(
+    r'perfil-administrativo',
+    PerfilAdministrativoViewSet,
+    basename='perfil-administrativo'
+)
+
+urlpatterns = router.urls

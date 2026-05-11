33a2049844ee62ecf64870b9cfe83b738991549b
Kauê Kluska <kauekluska@gmail.com>
2026-05-10 21:44:33 -0300

    esqueleto da pagina incial

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,10 +1,15 @@
-import "./header.css"
+import "./header.css";
+
 function Header() {
   return (
-    <header>
-      <h1>Acapra</h1>
-      <nav>
-        <a href="/">Início</a>
+    <header className="site-header">
+      <h1 className="site-brand">
+          <img className="logo-nav" src="/logo.png" alt="Logo" />
+          <p className="site-brand-name">ACAPRA</p>
+      </h1>
+      <nav className="site-nav" aria-label="Navegacao principal">
+        <a className="site-nav-link" href="/">Inicio</a>
+        <a className="site-nav-link" href="/">Login</a>
       </nav>
     </header>
   );
--- frontend/src/pages/HomeView/Home.jsx
+++ frontend/src/pages/HomeView/Home.jsx
@@ -12,8 +12,24 @@ function Home() {
 
   return (
     <div className="main">
-      <h1>Home</h1>
-      <p>{msg}</p>
+      <section className="hero">
+        <img className="hero-img" src="/hero.jpg" alt="" />
+
+        <div className="hero-content">
+          <h1 className="hero-text">
+            Associação Joaquinense de Proteção aos Animais
+          </h1>
+
+          <p className="hero-subtitle">
+            Resgatamos, cuidamos e encontramos famílias para cães abandonados.
+          </p>
+
+          <div className="hero-links">
+            <a href="#" className="link">Adotar</a>
+            <a href="#" className="link">Doar</a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }

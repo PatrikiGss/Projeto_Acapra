ad7e7593745cad495eb39d5c042a11206a42eb64
Mateus <mateuscorreiacoelho706@gmail.com>
2026-05-12 23:33:21 -0300

    Ajuste tela home e header

--- frontend/src/pages/HomeView/Home.jsx
+++ frontend/src/pages/HomeView/Home.jsx
@@ -1,19 +1,31 @@
 import { useEffect, useState } from "react";
-import "./Home.css"
+import "./Home.css";
 
 function Home() {
-  const [msg, setMsg] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:8000/")
-      .then(res => res.json())
-      .then(data => setMsg(data.message));
-  }, []);
-
   return (
     <div className="main">
-      <h1>Home</h1>
-      <p>{msg}</p>
+      <section className="hero">
+        <img
+          className="hero-img"
+          src="/hero-animals.png"
+          alt="Cães da associação"
+        />
+
+        <div className="hero-content">
+          <h1 className="hero-text">
+            Associação Joaquinense de Proteção aos Animais
+          </h1>
+
+          <p className="hero-subtitle">
+            Resgatamos, cuidamos e encontramos famílias para cães abandonados.
+          </p>
+
+          <div className="hero-links">
+            <a href="#" className="link">Adotar</a>
+            <a href="#" className="link">Doar</a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }

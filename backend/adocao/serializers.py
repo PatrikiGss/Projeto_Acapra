8101909a2f40e01ae6d64f14229e87e2eb63fea1
PatrikiGss <patriki7771@gmail.com>
2026-05-12 22:05:11 -0300

    testes.py e pequenos ajutes na outras classes de adoção.

--- backend/adocao/serializers.py
+++ backend/adocao/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Animal
 
-class AnimalSerializers(serializers.ModelSerializer):
+class AnimalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Animal
         fields = "__all__"

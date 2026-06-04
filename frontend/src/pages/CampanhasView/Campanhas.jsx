31a6f4da29058ee92637841c56855f08161be70e
Kauê Kluska <kauekluska@gmail.com>
2026-06-03 22:10:27 -0300

    feat: paginas de noticias resgates e campanhas

---
+++ frontend/src/pages/CampanhasView/Campanhas.jsx
@@ -0,0 +1,13 @@
+import NewsFeed from "../../components/NewsFeed/NewsFeed";
+
+function Campanhas() {
+  return (
+    <NewsFeed
+      categoria="campanhas"
+      titulo="Campanhas"
+      subtitulo="Acompanhe ações, mobilizações e chamadas importantes da ACAPRA."
+    />
+  );
+}
+
+export default Campanhas;

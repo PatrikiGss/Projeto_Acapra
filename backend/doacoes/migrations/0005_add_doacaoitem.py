41291f97d8f5784476c8ca50cf24e3c1fa2068f3
Kauê Kluska <kauekluska@gmail.com>
2026-06-14 14:45:30 -0300

    versao final

--- backend/doacoes/migrations/0005_add_doacaoitem.py
+++ backend/doacoes/migrations/0005_add_doacaoitem.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("doacoes", "0004_alter_dadospix_qr_code"),
+        ("doacoes", "0001_initial"),
     ]
 
     operations = [
